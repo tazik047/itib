0f269f0e0cf8a9f2f74942283331dc325a84aa33
Stas <stas249501@gmail.com>
2015-03-14 01:29:39 +0200

    commit

--- themes/maxx/tpl/field--field-tags--blog.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="clear"></div>
-<div class="sp"></div>
-<div class="post-tags"> <strong>Tags:</strong>
-<?php foreach ($items as $delta => $item) : ?>
-    <?php print render($item); ?>
-<?php endforeach; ?>
-</div>
-<div class="clear"></div>
--- themes/maxx/tpl/views-view-fields--portfolio--block.tpl.php
+++
@@ -1,1 +0,0 @@
-<p><?php print $fields['body']->content?></p>
